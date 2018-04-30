7770c12b071b6b21fabcba23d55ac2bd2470bc51
Carlita <carla.cutte@hotmail.com>
2018-04-30 01:21:00 -0600

    Fin

---
+++ Graficos2D/app/src/main/java/mx/edu/ittepic/graficos2d/MainActivity.java
@@ -0,0 +1,129 @@
+package mx.edu.ittepic.graficos2d;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int[] numeros={1,2,3,4,5,6,7,8,9,10,11,12};//{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60};
+    private Rect rect = new Rect();
+    private int radio=0;
+    int sX,sY,mX,mY,hX,hY;
+    CountDownTimer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PapelView papel = new PapelView(this);
+
+        setContentView(papel);
+    }
+
+
+    private class PapelView extends View {
+
+
+
+
+        public PapelView(Context context) {
+            super(context);
+            sX=sY=mX=mY=hX=hY=0;
+            final int[] s = {5};
+            final int[] m={12};
+            final int[] h={1};
+            timer=new CountDownTimer(61000,1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    double angle = Math.PI / 30 * (s[0] - 16);
+                    sX = (int) (1080 / 2 + Math.cos(angle) * radio - rect.width() / 2);
+                    sY = (int) (1080 / 2 + Math.sin(angle) * radio - rect.width() / 2);
+                    double angle2 = Math.PI / 30 * (m[0] - 16);
+                    mX = (int) (1080 / 2 + Math.cos(angle2) * radio - rect.width() / 2);
+                    mY = (int) (1080 / 2 + Math.sin(angle2) * radio - rect.width() / 2);
+                    double angle3 = Math.PI / 6 * (h[0] - 3);
+                    hX = (int) (1080 / 2 + Math.cos(angle3) * radio - rect.width() / 2);
+                    hY = (int) (1080 / 2 + Math.sin(angle3) * radio - rect.width() / 2);
+                    s[0]=s[0]+1;
+                    invalidate();
+
+                }
+
+                @Override
+                public void onFinish() {
+                    //Se ejecuta al finalizar el conteo
+                    invalidate();
+                    s[0]= 0;
+                    m[0]=m[0]+1;
+                    if(m[0]%60==0){
+                        h[0]=h[0]+1;
+                    }
+                    timer.start();
+
+                }
+            };
+            timer.start();
+        }
+
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            // Objeto Paint
+            Paint paint = new Paint();
+            paint.setColor(Color.BLACK);
+
+            // Pintar el canvas
+            canvas.drawPaint(paint);
+
+            // Obtener dimensiones
+            int ancho = canvas.getWidth();
+            int alto = canvas.getHeight();
+            paint.setColor(Color.WHITE);
+            paint.setTextSize(50);
+            paint.setAntiAlias(true);
+
+            // Texto
+            canvas.drawText("ancho" + ancho + "altura" + alto, 30, 30, paint);
+
+            // Lineas
+            paint.setColor(Color.WHITE);
+            canvas.drawLine(0, 40, ancho, 40, paint);
+            canvas.drawLine(20, 0, 20, alto, paint);
+
+            paint.setTextSize(50);
+
+            int min = Math.min(alto,ancho);
+            radio=(min/2)-100;
+
+            for (int n : numeros) {
+                String tmp = String.valueOf(n);
+                paint.getTextBounds(tmp, 0, tmp.length(), rect);
+                double angle = Math.PI / 6 * (n - 3);
+
+                int x = (int) (ancho / 2 + Math.cos(angle) * radio - rect.width() / 2);
+                int y = (int) (ancho / 2 + Math.sin(angle) * radio - rect.width() / 2);
+
+                canvas.drawText(tmp, x, y, paint);
+            }
+            //Segundos
+            canvas.drawLine(ancho/2,ancho/2,sX,sY,paint);
+            //Minutos
+            canvas.drawLine(ancho/2,ancho/2,mX,mY,paint);
+            //Horas
+            canvas.drawLine(ancho/2,ancho/2,hX,hY,paint);
+        }
+
+
+    }
+
+}
+
+
+
